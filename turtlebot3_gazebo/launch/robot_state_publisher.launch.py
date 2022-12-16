1ca9757d4394ef42074cd775dc47c8a6497bca35
MrKeith99 <kvalentincardenas@hotmail.com>
2022-12-16 13:24:03 +0900

    support for previous tb3 models

--- turtlebot3_gazebo/launch/robot_state_publisher.launch.py
+++ turtlebot3_gazebo/launch/robot_state_publisher.launch.py
@@ -31,35 +31,67 @@ def generate_launch_description():
     TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    urdf_file_name = 'turtlebot3_' + TURTLEBOT3_MODEL + '.urdf.xacro'
-    # urdf_file_name = 'turtlebot3_' + TURTLEBOT3_MODEL + '.urdf'
 
-    print('urdf_file_name : {}'.format(urdf_file_name))
+    if TURTLEBOT3_MODEL == 'big_wheel':
+        urdf_file_name = 'turtlebot3_' + TURTLEBOT3_MODEL + '.urdf.xacro'
 
-    urdf_path = os.path.join(
-        get_package_share_directory('turtlebot3_description'),
-        # get_package_share_directory('turtlebot3_gazebo'),
-        'urdf',
-        urdf_file_name)
+        print('urdf_file_name : {}'.format(urdf_file_name))
 
-    with open(urdf_path, 'r') as infp:
-        robot_desc = infp.read()
+        urdf_path = os.path.join(
+            get_package_share_directory('turtlebot3_description'),
+            # get_package_share_directory('turtlebot3_gazebo'),
+            'urdf',
+            urdf_file_name)
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
+        with open(urdf_path, 'r') as infp:
+            robot_desc = infp.read()
 
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[{
-                'use_sim_time': use_sim_time,
-                'robot_description': ParameterValue(Command(['xacro ', str(urdf_path)]), value_type=str),
-            }],
-            arguments=[urdf_path],
-        ),
-    ])
+        return LaunchDescription([
+            DeclareLaunchArgument(
+                'use_sim_time',
+                default_value='false',
+                description='Use simulation (Gazebo) clock if true'),
+
+            Node(
+                package='robot_state_publisher',
+                executable='robot_state_publisher',
+                name='robot_state_publisher',
+                output='screen',
+                parameters=[{
+                    'use_sim_time': use_sim_time,
+                    'robot_description': ParameterValue(Command(['xacro ', str(urdf_path)]), value_type=str),
+                }],
+                arguments=[urdf_path],
+            ),
+        ])
+
+    else:
+        urdf_file_name = 'turtlebot3_' + TURTLEBOT3_MODEL + '.urdf'
+
+        print('urdf_file_name : {}'.format(urdf_file_name))
+
+        urdf_path = os.path.join(
+            get_package_share_directory('turtlebot3_gazebo'),
+            'urdf',
+            urdf_file_name)
+
+        with open(urdf_path, 'r') as infp:
+            robot_desc = infp.read()
+
+        return LaunchDescription([
+            DeclareLaunchArgument(
+                'use_sim_time',
+                default_value='false',
+                description='Use simulation (Gazebo) clock if true'),
+
+            Node(
+                package='robot_state_publisher',
+                executable='robot_state_publisher',
+                name='robot_state_publisher',
+                output='screen',
+                parameters=[{
+                    'use_sim_time': use_sim_time,
+                    'robot_description': robot_desc
+                }],
+            ),
+        ])
